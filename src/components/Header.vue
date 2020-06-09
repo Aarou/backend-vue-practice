7931a11f8a7f5d88900f3664dd0db4e765882bf4
Aaron Lucien <aarou@Apples-iMac-2.local>
2020-06-09 08:21:21 -0400

    make the header component

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="header">
+        <header>
+            <h1>Header Content</h1>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+.header {
+    text-align: center;
+    background: black;
+}
+</style>
